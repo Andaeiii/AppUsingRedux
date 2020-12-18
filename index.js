f970802c9c50a367192fa44d9ac2d3662c3a9113
Ande Caleb <andaeiii@aol.com>
2020-12-18 14:09:46 +0100

    a little update defining the processes..

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const createStore = redux.createStore;
 
 const BUY_CAKE = 'BUY_CAKE';
 
-//action is defined already.. 
+//action creators... here.  is defined already.. 
+//you could pass the object itself to the dispatch method.. 
+//but using functions is better... 
 function buyCake(){
     return {
         type: BUY_CAKE,
@@ -53,10 +55,21 @@ console.log('initial state', store.getState());
 const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()));
 
 // used to update the state... dispatch(action) // action = buy state.
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-store.dispatch(buyCake())
+// to simulate a process multiple times... 
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
 
 
 //unsubscribe by calling the function returned by the subscribe method.. 
 unsubscribe();
+
+
+// so the redux pattern is as follows.... 
+// ------------------------------------------------
+// you create a store
+// declare the action, state, reducer.. 
+// define your actions and the action creators
+// subscribe to the store
+// dispatch actions to update the store.. 
+// finally unsubscribe to the changes..... 
