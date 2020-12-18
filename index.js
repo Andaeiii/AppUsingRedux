087448c11f2f4b8a971fef6757b4595f4b28a8c3
Ande Caleb <andaeiii@aol.com>
2020-12-18 14:02:27 +0100

    the skeletal redux pattern..

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-console.log('from index.js... ');
+const redux  = require('redux');
+const createStore = redux.createStore;
+
+const BUY_CAKE = 'BUY_CAKE';
+
+//action is defined already.. 
+function buyCake(){
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+
+// {previous State, action } => newState
+// application state = a simple object.. .
+
+//the state of the application.... 
+const initialState = {
+    numOfCakes : 10
+}
+
+//the reducer... here.... 
+// the reducer function controls how the state transitions happen....
+
+const reducer = (state=initialState, action) => {
+    switch(action.type){
+        case BUY_CAKE: 
+            return{
+                ...state,                           // copy the state object... 
+                numOfCakes: state.numOfCakes - 1    // then update this property in the object.. 
+            }
+            break;
+        default: return state
+    }
+
+}
+
+
+// the Redux Store..
+/*  holds the application state
+    allows the state to be updated via dispatch... 
+    allows the app. to register listeners via subscribe... 
+    you can also unsubscribe to the store, by the subscribe functions..
+*/
+
+const store = createStore(reducer);
+
+console.log('initial state', store.getState());
+
+// allow the app to subscribe to changes in the store...  { unsubscribe = value returned }
+// so anytime the state updates, it is logged to the console... 
+const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()));
+
+// used to update the state... dispatch(action) // action = buy state.
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+
+
+//unsubscribe by calling the function returned by the subscribe method.. 
+unsubscribe();
